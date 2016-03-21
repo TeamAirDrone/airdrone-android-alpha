ce7e77da07a202c7b24e2be658bd2f4615134f20
TeamAirDrone <airdroneaustria@gmail.com>
2016-03-21 04:06:31 +0100

    An Android app for autonomously flying drones.

---
+++ app/src/main/java/com/stonedpotatoproductions/roots/MainActivity.java
@@ -0,0 +1,216 @@
+package com.stonedpotatoproductions.roots;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    public final static String EXTRA_LAT = "com.stonedpotatoproductions.Roots.MESSAGE";
+    public final static String EXTRA_LNG = "com.stonedpotatoproductions.Roots.MESSAGE";
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    double laValue;
+    double lnValue;
+
+    Button locanzeigen;
+    EditText lonlatText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        locanzeigen = (Button) findViewById(R.id.button_maps);
+        lonlatText = (EditText) findViewById(R.id.lnglatText);
+        setSupportActionBar(toolbar);
+
+        locanzeigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = "Lat: " + lnValue + " Lng: "+ laValue;
+                lonlatText.setText(s);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        try {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+            else {
+                handleNewLocation(location);
+            }
+        }
+        catch(SecurityException ex)
+        {
+
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        //Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        laValue = location.getLatitude();
+        lnValue = location.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            //Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+
+    public void mkf_marker(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        EditText lat = (EditText) findViewById(R.id.txt_lat);
+        EditText lng = (EditText) findViewById(R.id.txt_lng);
+        double iLat = Double.parseDouble(lat.getText().toString());
+        double iLng = Double.parseDouble(lng.getText().toString());
+
+        Bundle b = new Bundle();
+        b.putDouble("lat", iLat);
+        b.putDouble("lng", iLng);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+
+
+    public void goDetails(View view){
+        Intent intent = new Intent(this, Details.class);
+        startActivity(intent);
+    }
+
+    public void goMaps(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        double iLat = laValue;
+        double iLng = lnValue;
+        Log.i("LatLng","Lat: "+ laValue+" Lng: "+lnValue);
+        Bundle b = new Bundle();
+        b.putDouble("lat", iLat);
+        b.putDouble("lng", iLng);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/stonedpotatoproductions/roots/MapsActivity.java
@@ -0,0 +1,297 @@
+package com.stonedpotatoproductions.roots;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationChangeListener;
+import com.google.android.gms.common.ConnectionResult;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMarkerDragListener,
+        OnMapLongClickListener, OnMarkerClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, GoogleMap.OnInfoWindowLongClickListener {
+
+    public static final String TAG = MapsActivity.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleMap mMap;
+    private UiSettings mUiSettings;
+    private int totalMarkers = 1;
+    double latValue = 20.0;
+    double lngValue = 20.0;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+                .compassEnabled(true)
+                .rotateGesturesEnabled(true)
+                .mapToolbarEnabled(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Intent intent = getIntent();
+
+        if(intent.getExtras() != null) {
+            Bundle b = intent.getExtras();
+            latValue = b.getDouble("lat");
+            lngValue = b.getDouble("lng");
+        }else{
+            latValue = 48.00;
+            lngValue = 16.00;
+        }
+
+        mMap = googleMap;
+        mUiSettings = mMap.getUiSettings();
+        // Add a marker in Sydney and move the camera
+
+        LatLng cords = new LatLng(latValue, lngValue);
+        Marker marker = mMap.addMarker(new MarkerOptions().position(cords).draggable(true).title("Marker Nr. " + totalMarkers));
+        marker.setSnippet("Position: " + marker.getPosition());
+        marker.showInfoWindow();
+        CameraUpdate myLocation = CameraUpdateFactory.newLatLngZoom(cords, 15);
+        mMap.animateCamera(myLocation);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnInfoWindowLongClickListener(this);
+
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setMapToolbarEnabled(true);
+
+
+    }
+
+    public void addMarker(View view){
+        totalMarkers = totalMarkers + 1;
+
+        LatLng center = mMap.getCameraPosition().target;
+        String cords = center.toString();
+        CameraUpdate myLocation = CameraUpdateFactory.newLatLngZoom(center, 15);
+        showToast();
+        mMap.animateCamera(myLocation);
+
+        Marker marker = mMap.addMarker(new MarkerOptions().position(center).draggable(true).title("Marker Nr. " + totalMarkers).snippet("Position: "+ cords));
+        marker.showInfoWindow();
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        totalMarkers = totalMarkers + 1;
+        String cords = latLng.toString();
+        Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).draggable(true).title("Marker Nr. " + totalMarkers).snippet("Position: " + cords));
+        CameraUpdate myLocation = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+        showToast();
+        mMap.animateCamera(myLocation);
+        marker.showInfoWindow();
+
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        marker.setSnippet("Position: " + marker.getPosition().toString());
+        marker.showInfoWindow();
+    }
+
+    public void showToast(){
+        Context context = getApplicationContext();
+        CharSequence text = "You added a marker!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        marker.setSnippet("Position: " + marker.getPosition().toString());
+        marker.showInfoWindow();
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        marker.setSnippet("Position: " + marker.getPosition().toString());
+        Log.i("Lat/Lng: ", marker.getPosition().toString());
+        marker.showInfoWindow();
+
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        try {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+            else {
+                handleNewLocation(location);
+            }
+        }
+        catch(SecurityException ex)
+        {
+
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        lngValue = location.getLatitude();
+        latValue = location.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+
+    @Override
+    public void onInfoWindowLongClick(Marker marker) {
+        marker.remove();
+        totalMarkers = totalMarkers - 1;
+    }
+};
+
+
